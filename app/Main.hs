aa6e9366a805f1f9d7e5ece7d1b73fa983089054
Janik Eriksson <lyannen258@web.de>
2021-08-15 15:48:33 +0200

    First parsing attempts

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
 
+import Text.Parsec
+import Parser
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    fileContent <- readFile "../Input/writing_a_statement.txt"
+    let output = parseFile fileContent
+    print output
+
+parseFile :: String -> String
+parseFile content = case parse coderunnerParser "" content of
+                Left a -> show a
+                Right b -> show b
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,1 +1,14 @@
 module Parser where
+
+import Text.Parsec
+import GHC.Show (Show)
+
+data AST = AST String [AST]
+    deriving (Show)
+
+
+coderunnerParser :: Parsec String () AST
+coderunnerParser = do
+    p <- string "Parameter:"
+    newline
+    return (AST p [])
