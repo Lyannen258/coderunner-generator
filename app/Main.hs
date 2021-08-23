d3e09b818839025512f922f89f12076c95d40749
Janik Eriksson <lyannen258@web.de>
2021-08-23 16:17:16 +0200

    Always generate AST for all three example files

--- app/Main.hs
+++ app/Main.hs
@@ -4,21 +4,32 @@ import Text.Parsec
 import Parser
 import Data.Tree(drawTree)
 import System.IO
+import System.FilePath (takeDirectory, takeBaseName)
 
 main :: IO ()
 main = do
-    inputHandle <- openFile "../Input/test.txt" ReadMode
+    let listOfPath = map ("../Input/" ++) [
+            "01_writing_a_statement.txt",
+            "02_missing_parts_of_code.txt",
+            "03_errors_in_the_code.txt"
+            ]
+    mapM_ parseFile listOfPath
+
+parseFile :: String -> IO ()
+parseFile filePath = do
+    inputHandle <- openFile filePath ReadMode
     hSetEncoding inputHandle utf8
     fileContent <- hGetContents inputHandle
 
-    let output = parseFile fileContent
+    let output = parseString fileContent
 
-    outputHandle <- openFile "../Input/ast.txt" WriteMode
+    let outputPath = takeDirectory filePath ++ "/AST_" ++ takeBaseName filePath
+    outputHandle <- openFile outputPath WriteMode
     hSetEncoding outputHandle utf8
     hPutStr outputHandle output
     hClose outputHandle
 
-parseFile :: String -> String
-parseFile content = case parse coderunnerParser "" content of
+parseString :: String -> String
+parseString content = case parse coderunnerParser "" content of
                 Left a -> show a
                 Right b -> drawTree $ toDataTree b
