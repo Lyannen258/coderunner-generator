eee235f7d987d7f938018532851e0fea1817d43c
Janik Eriksson <eriksson@wifa.uni-leipzig.de>
2022-05-03 13:15:52 +0200

    Rebuild Interface of generalized generator

--- src/CoderunnerGenerator/Interface.hs
+++ src/CoderunnerGenerator/Interface.hs
@@ -1,23 +1,12 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use newtype instead of data" #-}
-
 module CoderunnerGenerator.Interface where
 
-data ParserConfig = ParserConfig
-  { sections :: [String]
-    -- keywords :: [String]
-  }
+import CoderunnerGenerator.Types.Globals
+import CoderunnerGenerator.Main
+import Control.Monad.Trans.Reader
+import qualified CoderunnerGenerator.CmdArgs as CmdArgs
 
-class Runnable a where
-  run :: a -> (b -> String) -> IO ()
-
-instance Runnable (String -> a) where
-  run parser generator = do
-    putStrLn "Not yet implemented"
-    return ()
-
-instance Runnable ParserConfig where
-  run parser generator = do
-
-    return ()
+run :: (String -> (ParseResult, s)) -> ([Configuration] -> s -> [String]) -> IO ()
+run parser generator = handleErrors $ do
+  args <- CmdArgs.executeParser
+  let g = constructGlobals parser generator args
+  runReaderT main g
