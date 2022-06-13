e6f356811487658e9dce442281eca227103d5208
Janik Eriksson <eriksson@wifa.uni-leipzig.de>
2022-06-13 10:58:38 +0200

    Fix test module

--- src/CoderunnerGenerator/Test.hs
+++ src/CoderunnerGenerator/Test.hs
@@ -4,14 +4,14 @@ import CoderunnerGenerator.Types.ParseResult as PR
 
 simpleParseResultWithoutConstraints :: Either String ParseResult
 simpleParseResultWithoutConstraints = do
-  pr <- addParameter PR.empty (PR.singleParam "p1" [Final "v1", Final "v2", Final "v3"])
-  pr' <- addParameter pr (PR.singleParam "p2" [Final "v1", Final "v2"])
-  addParameter pr' (PR.singleParam "p3" [Final "v1", Final "v2", Final "v3"])
+  pr <- addParameter PR.empty (PR.makeParam "p1" [Final "v1", Final "v2", Final "v3"])
+  pr' <- addParameter pr (PR.makeParam "p2" [Final "v1", Final "v2"])
+  addParameter pr' (PR.makeParam "p3" [Final "v1", Final "v2", Final "v3"])
 
 simpleParseResult :: Either String ParseResult
 simpleParseResult = do
   pr <- simpleParseResultWithoutConstraints
   addConstraint
     pr
-    ("p1", PR.singleValue $ Final "v1")
-    ("p2", PR.singleValue $ Final "v1")
+    ("p1", PR.makeValue $ Final "v1")
+    ("p2", PR.makeValue $ Final "v1")
