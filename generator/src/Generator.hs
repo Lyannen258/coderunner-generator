e1b064e50577b2d4b1d53713e8033eb5dc8473de
Janik Eriksson <eriksson@wifa.uni-leipzig.de>
2022-06-22 16:45:48 +0200

    Remove handleerrors function

--- generator/src/Generator.hs
+++ generator/src/Generator.hs
@@ -1,13 +1,13 @@
 module Generator (run) where
 
-import qualified Generator.CmdArgs as CmdArgs
-import Generator.Main (main)
-import Generator.Configuration (Configuration)
-import Generator.Globals (constructGlobals)
-import Generator.ToParseResult
 import Control.Exception (SomeException, try)
 import Control.Monad.Trans.Except (runExceptT)
 import Control.Monad.Trans.Reader (runReaderT)
+import qualified Generator.CmdArgs as CmdArgs
+import Generator.Configuration (Configuration)
+import Generator.Globals (constructGlobals)
+import Generator.Main (main)
+import Generator.ToParseResult
 
 -- | r is the return that the run function works with,
 -- u is the user state that is fed to the generator function
@@ -21,19 +21,10 @@ type GeneratorFunction u =
 
 -- | The run function
 run :: (Show r, ToParseResult r) => ParserFunction r s -> GeneratorFunction s -> IO ()
-run parser generator = handleErrors $ do
+run parser generator = do
   args <- CmdArgs.executeParser
   let g = constructGlobals parser generator args
   res <- runExceptT (runReaderT main g)
   case res of
     Left s -> putStrLn $ "Error: " ++ s
     Right _ -> return ()
-
-handleErrors :: IO () -> IO ()
-handleErrors action = do
-  res <- resIO
-  case res of
-    Left e -> print $ show e
-    _ -> return ()
-  where
-    resIO = try action :: IO (Either SomeException ())
